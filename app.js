a29aeb76566f8a97e9d71a044caac72f00b1cf1c
oswi <oswaldo.adrian@live.com.mx>
2023-02-16 12:21:38 -0600

    tick every 5 seconds

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const cron = require('node-cron');
+
+let times = 0;
+
+cron.schedule('1-59/5 * * * * *', () => {
+  times++;
+  console.log('tick every 5 seconds', times);
+});
